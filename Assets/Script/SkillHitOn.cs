6520ab76d8f1e9b01a297a7b5fee7021e2f1f22a
hwannanyi <hwannan2@gmail.com>
2025-04-04 01:58:39 +0900

    hancan
    qq

--- Assets/Script/SkillHitOn.cs
+++ Assets/Script/SkillHitOn.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using static UnityEngine.GraphicsBuffer;
+using System.Linq;
 
 public class SkillHitOn : MonoBehaviour
 {
@@ -48,39 +50,61 @@ public class SkillHitOn : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        var manager = CharacterStats.Instance;
-        if (manager == null)
+        if (other.gameObject.tag=="Character")
         {
-            Debug.LogWarning("CharacterStats 매니저 인스턴스가 없습니다.");
-            return;
+            var manager = CharacterStats.Instance;
+            if (manager == null)
+            {
+                Debug.LogWarning("CharacterStats 매니저 인스턴스가 없습니다.");
+                return;
+            }
+
+            var target = other.transform.root.gameObject;
+            var self = caster.transform.root.gameObject;
+
+            if (target == self)
+            {
+                Debug.Log("[SkillHitOn] 자기 자신과 충돌 - 무시");
+                return;
+            }
+
+            var targetStats = manager.GetStats(target);
+            var casterStats = manager.GetStats(self);
+
+            if (targetStats == null)
+            {
+                Debug.LogWarning($" '{target.name}' 은 Stats 정보 없음");
+                return;
+            }
+
+            if (casterStats == null)
+            {
+                Debug.LogWarning($" caster '{self.name}' 은 Stats 정보 없음");
+                return;
+            }
+
+            // 여기서 팀 비교 등 처리
+            Debug.Log($"'{target.name}' 가 '{caster.name}' 의 스킬에 피격됨!");
+            if (skillData.skillTarget.Contains(Target.enemy))
+            {
+                if (targetStats.team != casterStats.team)
+                {
+                    int damage = Mathf.RoundToInt(skillData.basicValue);
+                    targetStats.hp -= damage;
+                    Debug.Log($"[Hit] {targetStats.name}이(가) {damage} 피해를 입음. 남은 HP: {targetStats.hp}");
+                }
+            }
         }
 
-        var target = other.transform.root.gameObject;
-        var self = caster.transform.root.gameObject;
-
-        if (target == self)
+        if(other.gameObject.tag == "Tile")
         {
-            Debug.Log("[SkillHitOn] 자기 자신과 충돌 - 무시");
-            return;
+            Debug.Log("타일에 충돌!");
         }
 
-        var targetStats = manager.GetStats(target);
-        var casterStats = manager.GetStats(self);
-
-        if (targetStats == null)
+        if (other.gameObject.tag == "skill")
         {
-            Debug.LogWarning($" '{target.name}' 은 Stats 정보 없음");
-            return;
+            Debug.Log("스킬에 충돌!");
         }
-
-        if (casterStats == null)
-        {
-            Debug.LogWarning($" caster '{self.name}' 은 Stats 정보 없음");
-            return;
-        }
-
-        // 여기서 팀 비교 등 처리
-        Debug.Log($"'{target.name}' 가 '{caster.name}' 의 스킬에 피격됨!");
     }
 
     /*if (targetStats.team != casterStats.team)
